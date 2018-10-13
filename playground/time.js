bb2ee0ec4ff47c239df768bc8d529554455df1de
DHAVAL <dhaval78dp@gmail.com>
2018-10-13 11:31:50 +0530

    add message time

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+var date = moment();
+
+// console.log(date.format('MMM Do, YYYY'));
+console.log(date.format('h:mm a'));
+// var date = new Date();
+// var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nev','Dec'];
+
+// console.log(months[date.getMonth()]);
